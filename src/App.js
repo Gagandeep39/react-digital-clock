acd0d27f85f34975a463793814e732a775fb2729
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 22:54:35 +0530

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/Header/HeaderComponent';
 import ToggleButtonComponent from './components/ToggleButton/ToggleButtonComponent';
