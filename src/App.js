77d288eb5df1fd5ab5b285212fce6a8111e50ed2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-26 19:37:55 +0530

    Added toggle button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/Header/HeaderComponent';
+import ToggleButtonComponent from './components/ToggleButton/ToggleButtonComponent';
 
 class App extends Component {
   render () {
     return (
       <div>
         <HeaderComponent />
+        <ToggleButtonComponent />
       </div>
     );
   }
